03611eba20bb16a2093db416b8facae7147c3c46
Mdrs-Arthur <medeiros.arthur1@escolar.ifrn.edu.br>
2025-12-04 22:55:34 -0300

    ajuste

--- Prática Max 10/v8/sons.py
+++ Prática Max 10/v8/sons.py
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+import pygame
+import os
 import pygame
 import os
 
@@ -34,40 +35,3 @@ class Tema:
     def som_tema(self):
         if not pygame.mixer.music.get_busy():
             pygame.mixer.music.play(-1)
-=======
-import pygame
-import os
-
-BASE_DIR = os.path.dirname(__file__)
-SND_DIR = os.path.join(BASE_DIR, "sounds")
-
-class Sons:
-    @staticmethod
-    def som_morte():
-        morte = pygame.mixer.Sound(os.path.join(SND_DIR, "inimorrendo.mp3"))
-        morte.set_volume(1.0)
-        morte.play()
-
-    @staticmethod
-    def som_buff():
-        buff = pygame.mixer.Sound(os.path.join(SND_DIR, "pegandobuff.mp3"))
-        buff.set_volume(1.0)
-        buff.play()
-
-    @staticmethod
-    def som_tiro():
-        tiro = pygame.mixer.Sound(os.path.join(SND_DIR, "tiropadrao.mp3"))
-        tiro.set_volume(1.0)
-        tiro.play()
-
-
-class Tema:
-    def __init__(self):
-        pygame.mixer.init()
-        pygame.mixer.music.load(os.path.join(SND_DIR, "musica.mp3"))
-        pygame.mixer.music.set_volume(0.8)
-
-    def som_tema(self):
-        if not pygame.mixer.music.get_busy():
-            pygame.mixer.music.play(-1)
->>>>>>> a4a250e03b97e68b90d972d4db7de9c6178b646c
